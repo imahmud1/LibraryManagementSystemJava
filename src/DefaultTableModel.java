8ab86846b6a0cb0e89035026eed418565d0629ba
Iftikhar Mahmud <iftikhar.mahmud101@gmail.com>
2022-07-28 05:18:43 +0600

    Initial Commit

---
+++ src/DefaultTableModel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IFTI
+ */
+class DefaultTableModel {
+
+    void setRowCount(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addRow(String[] row) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
